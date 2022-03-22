f6f1366117542fbb0268f0d762e34390e2f3f6de
kxthii <katharina.gruber@bulme.at>
2022-03-22 09:44:32 +0100

    app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, redirect, request, flash, session
+from flask.templating import render_template
+#from editItemFrom import EditItemForm
+from model import db, Todoitem
+#from addItemForm import AddItemForm
+#from deleteItemForm import DeleteItemForm
+
+app = Flask(__name__)
+app.secret_key = "VerySecretSecretKey"
+
+#Datenbankzugriff konfigurieren
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost/Sport"
+db.init_app(app)
