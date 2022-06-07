cf180d29d186044cdab45b2954d3b8074ebf58ad
kxthii <katharina.gruber@bulme.at>
2022-06-07 10:23:13 +0200

    07.06.2022

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask_wtf.csrf import CSRFProtect
 app = Flask(__name__)
 app.secret_key = "VerySecretSecretKey"
 
-#Datenbankzugriff konfigurieren
+# Datenbankzugriff konfigurieren
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost/Sport"
 
@@ -18,7 +18,7 @@ csrf = CSRFProtect(app)
 
 db.init_app(app)
 
-#hier blueprint registrieren
+# hier blueprint registrieren
 app.register_blueprint(index_blueprint)
 app.register_blueprint(sportart_blueprint)
 app.register_blueprint(sportgruppe_blueprint)
